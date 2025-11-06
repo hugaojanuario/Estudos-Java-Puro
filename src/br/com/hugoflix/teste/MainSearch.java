2146da0e07f8af7041f06ea24ca6e0b7158b9002
Hugo Januario <128010237+hugaojanuario@users.noreply.github.com>
2025-11-06 20:25:02 -0300

    consumindo Api

---
+++ src/br/com/hugoflix/teste/MainSearch.java
@@ -0,0 +1,22 @@
+package br.com.hugoflix.teste;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class MainSearch {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://www.omdbapi.com?t=matrix&apikey=e4702f78"))
+                .build();
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
